02ec9fec2388c634a985ba70b40cc4557fa86f8f
Bianca-Apostolescu <bianca.apostolescu@stud.energ.upb.ro>
2021-08-07 03:23:48 -0700

    Working Text Display Driver

---
+++ kernel/drivers/src/dots_text_display.rs
@@ -0,0 +1,251 @@
+use kernel::hil::led::Led;
+use kernel::process::{Error, ProcessId};
+use kernel::syscall::{CommandReturn, SyscallDriver};
+use kernel::{debug, ErrorCode};
+use kernel::hil::time::{Alarm, AlarmClient, ConvertTicks};
+use kernel::grant::Grant;
+use kernel::processbuffer::{ReadOnlyProcessBuffer, ReadableProcessBuffer};
+use core::cell::Cell;
+use kernel::errorcode::into_statuscode;
+
+
+pub const DRIVER_NUM: usize = 0xa0002;
+
+const DIGITS: [u32; 10] = [
+    0b11111_10011_10101_11001_11111, // 0
+    0b00100_01100_00100_00100_01110, // 1
+    0b11110_00001_01110_10000_11111, // 2
+    0b11110_00001_11110_00001_11110, // 3
+    0b10000_10000_10100_11111_00100, // 4
+    0b11111_10000_11110_00001_11110, // 5
+    0b11111_10000_11111_10001_11111, // 6
+    0b11111_00001_00010_00100_00100, // 7
+    0b11111_10001_11111_10001_11111, // 8
+    0b11111_10001_11111_00001_11111, // 9
+];
+
+
+// Implementare default (valoare initiala ca sa nu crape) pt AppData
+#[derive (Default)] 
+pub struct AppData {
+    buffer: ReadOnlyProcessBuffer,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum State {
+    Idle, 
+    Printing {
+        process_id: ProcessId,
+        ms: usize,
+        position: usize,
+        len: usize,
+
+    },
+}
+
+pub struct DotsTextDisplay<'a, L: Led, A: Alarm<'a>> {
+    leds: &'a [&'a L; 25],
+    alarm: &'a A,
+    app_data: Grant<AppData, 1>, // tip si nr subscribe
+    state: Cell<State>,
+}
+
+impl<'a, L: Led, A: Alarm<'a>> DotsTextDisplay<'a, L, A> {
+    
+    pub fn new(
+        leds: &'a [&'a L; 25],
+        alarm: &'a A,
+        app_data: Grant<AppData, 1>,
+    
+    ) -> DotsTextDisplay<'a, L, A> {
+        
+        DotsTextDisplay { 
+            leds, 
+            alarm, 
+            app_data, 
+            state: Cell::new(State::Idle), 
+        }
+    }
+
+    fn display_next_digit (&self) -> Result<(), ErrorCode> {
+
+        match self.state.get() {
+            
+            State::Printing {
+                process_id,
+                ms,
+                position,
+                len,
+            } => {
+
+                let res = self.app_data.enter(process_id, |data, upcalls| {
+                    let res = data.buffer.enter (|buffer| {
+                       
+                        if position < buffer.len() && position < len { // NECLAR
+                            self.display(buffer[position].get() as char);
+
+                            self.state.set (State::Printing {
+                                process_id,
+                                ms,
+                                position: position + 1,
+                                len,
+                            });
+
+                            true
+                        }
+                        else {
+                            upcalls.schedule_upcall(0, (0, 0, 0)).ok();
+                            self.state.set(State::Idle);
+                            false
+                        }
+                        
+                });
+                // Match cu o singura ramura 
+                if let Err(error) = res { 
+
+                    upcalls.schedule_upcall(0, (into_statuscode(error.into()), 0, 0)).ok();
+                };
+                res
+
+            });
+
+        match res {
+
+            Ok(Ok(next_print)) => {
+
+                if next_print {
+                    self.alarm.set_alarm(self.alarm.now(), self.alarm.ticks_from_ms(ms as u32));
+                }
+                
+                Ok(())
+            },
+
+            // DIFERENTA Ok(Err(error)) VS Err(error) !!!
+            Ok(Err(error)) => {
+                self.state.set(State::Idle);
+                Err(error.into())
+            },
+
+            Err(error) => {
+                self.state.set(State::Idle);
+                Err(error.into())
+            },
+        }
+    },
+
+        State::Idle => {
+            Err(ErrorCode::FAIL)
+            
+            // SAU mai bine pentru Rust
+            // unreachable!()
+        },    
+    }
+        
+}
+    fn display(&self, digit: char) {
+        let digit_index = digit as usize - '0' as usize;
+        let current_digit = DIGITS[digit_index];
+
+        for index in 0..25 {
+            let bit = (current_digit >> (24 - index)) & 0x1; // DE CE FACE SI CU 0x1
+
+            if bit == 1 {
+                self.leds[index].on();
+            }
+            else {
+                self.leds[index].off();
+            }
+        }
+
+    }
+    
+}
+
+impl<'a, L: Led, A: Alarm<'a>> SyscallDriver for DotsTextDisplay<'a, L, A> {
+    
+    fn command(
+        &self,
+        command_num: usize,
+        r2: usize,
+        r3: usize,
+        process_id: ProcessId,
+    ) -> CommandReturn {
+        
+        match command_num {
+            0 => CommandReturn::success(),
+            
+            // Print digit
+            1 =>  {
+                if self.state.get() == State::Idle {
+                    self.state.set (State::Printing {
+                        process_id,
+                        ms: r3,
+                        position: 0,
+                        len: r2
+                    });
+
+                    if let Err(error) = self.display_next_digit() { // match cu o singura ramura
+                        CommandReturn::failure(error)
+                    }
+                    else {
+                        CommandReturn::success()
+                    }
+
+                    // Se poate inlocui if - ul cu urm structura:
+                    // match self.display_next_digit() {
+                    //     Err(error) => // ....
+                    //     _ => // ....
+                    // }
+                    
+                }
+                else {
+                    CommandReturn::failure(ErrorCode::BUSY)
+                }
+                    
+            },
+                
+            _ => CommandReturn::failure(ErrorCode::NOSUPPORT),
+
+        }
+    }
+
+    fn allocate_grant(&self, process_id: ProcessId) -> Result<(), Error> {
+        self.app_data.enter(process_id, |_, _| {})
+    }
+
+    fn allow_readonly (
+        &self,
+        process_id: ProcessId,
+        allow_num: usize,
+        mut buffer: ReadOnlyProcessBuffer,
+
+    ) -> Result<ReadOnlyProcessBuffer, (ReadOnlyProcessBuffer, ErrorCode)> {
+        
+        match allow_num {
+            0 => {
+                let res = self.app_data.enter (process_id, |data, _| {
+                    core::mem::swap(&mut data.buffer, &mut buffer);
+                });
+
+                match res {
+                    Ok(()) => Ok(buffer),
+
+                    Err(error) => Err((buffer, error.into())),
+                }
+            },
+
+            _ => Err((buffer, ErrorCode::NOSUPPORT)),
+        }
+    }
+}
+
+impl<'a, L: Led, A: Alarm<'a>> AlarmClient for DotsTextDisplay<'a, L, A> {
+    
+    fn alarm(&self) {
+        
+        debug! ("Fired");
+        let _ = self.display_next_digit();
+            
+
+    }
+}
